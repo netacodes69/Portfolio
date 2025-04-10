29856d3cb980d3769fd995ba47ec17c406374686
netacodes69 <utkarsh.2023ug2037@iiitranchi.ac.in>
2025-04-10 22:26:36 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,9 +23,19 @@ const Hero = () => {
             Contact With Me
           </button>
         </a>
-            <button
-            className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
-            transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Resume</button>
+          <a
+              href="https://drive.google.com/file/d/1aaDlzaeluNVCpy8EvaAeWV27jaRYgUoW/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+          <button
+              className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
+              transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
+            >
+            Resume
+          </button>
+      </a>
+
         </div>
 
     </div>
